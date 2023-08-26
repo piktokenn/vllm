2f45a1c5591599ce7d1dbb35890b47167807fe5a
piktokenn <120285373+piktokenn@users.noreply.github.com>
2023-08-26 20:46:21 +0300

    Update api_server.py

--- vllm/entrypoints/api_server.py
+++ vllm/entrypoints/api_server.py
@@ -69,7 +69,7 @@ async def generate(request: Request) -> Response:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--host", type=str, default="0.0.0.0")
     parser.add_argument("--port", type=int, default=8000)
     parser = AsyncEngineArgs.add_cli_args(parser)
     args = parser.parse_args()
